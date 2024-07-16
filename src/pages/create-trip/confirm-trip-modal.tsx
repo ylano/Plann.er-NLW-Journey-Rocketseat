2ae3ed9dacf04cae37e5b1428e98f35e4fcc6a6a
ylano <franciscoylano@gmail.com>
2024-07-16 14:13:56 -0300

    first commit

---
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -0,0 +1,62 @@
+import { User, X } from "lucide-react"
+import type { FormEvent } from "react"
+import { Button } from "../../components/button"
+
+interface ConfirmTripModalProps{
+  closeConfirmTripModal: () => void
+  setOwnerName: (name: string) => void
+  setOwnerEmail: (name: string) => void
+  createTrip: (event: FormEvent<HTMLFormElement>) => void
+}
+
+export function ConfirmTripModal({
+  closeConfirmTripModal,
+  createTrip,
+  setOwnerName,
+  setOwnerEmail,
+}: ConfirmTripModalProps) {
+  return (
+    <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
+    <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+    <div className='space-y-2'>
+      <div className='flex items-center justify-between'>
+        <h2 className='text-lg font-semibold'>Confirmar criação de viagem</h2>
+        <button onClick={closeConfirmTripModal} type='button'>
+          <X className='size-5 text-zinc-400'/>
+        </button>
+      </div>
+      <p className='text-sm text-zinc-400'>
+        Para concluir a criação de viagem para <span className='font-semibold text-zinc-100'> Fortaleza, Ceará </span> nas datas de <span className='font-semibold text-zinc-100'>16 a 27 de Agosto de 2024</span> preencha seus dados abaixo:
+      </p>
+      </div>
+        <form onSubmit={createTrip} className='space-y-3'>
+          <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+            <User className='size-5 text-zinc-400'/>
+            <input  
+            name='name' 
+            placeholder='Seu nome completo' 
+            className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" 
+            onChange={event => setOwnerName(event.target.value)}
+            />
+            
+          </div>
+          <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+            <User className='size-5 text-zinc-400'/>
+            <input 
+            type="email" 
+            name='email' 
+            placeholder='Seu e-mail pessoal' 
+            className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+            onChange={event => setOwnerEmail(event.target.value)}
+            />
+          </div> 
+ 
+            <Button type="submit" variant="primary" size="full">
+              Confirmar criação de viagem
+            </Button>
+        </form>
+    </div>
+    
+  </div>
+  )
+}
---
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -0,0 +1,94 @@
+import { ArrowRight, Calendar, MapPin, Settings2, X } from "lucide-react";
+import { Button } from "../../../components/button";
+import { useState } from "react";
+import { DayPicker, type DateRange } from "react-day-picker";
+import {format} from 'date-fns'
+import "react-day-picker/dist/style.css";
+
+interface DestinationAndDateStepProps{
+  isGuestsInputOpen: boolean
+  eventStartAndEndDates: DateRange | undefined
+  closeGuestsInput: () => void
+  openGuestsInput: () => void
+  setDestination: (destination: string) => void
+  setEventStartAndEndDates: (dates: DateRange | undefined) => void
+}
+
+export function DestinationAndDateStep({
+  isGuestsInputOpen,
+  closeGuestsInput,
+  openGuestsInput,
+  setDestination,
+  setEventStartAndEndDates,
+  eventStartAndEndDates,
+}: DestinationAndDateStepProps){
+
+  const[isDatePickerOpen, setIsDatePickerOpen] = useState(false)
+
+  function openDatePicker(){
+    return setIsDatePickerOpen(true)
+  }
+  
+  function closeDatePicker(){
+    return setIsDatePickerOpen(false)
+  }
+  
+  const displayedDate = eventStartAndEndDates && eventStartAndEndDates.from && eventStartAndEndDates.to 
+  ? format(eventStartAndEndDates.from, "d' de 'LLL").concat(' até ').concat(format(eventStartAndEndDates.to, "d' de 'LLL"))
+  : null
+
+  return (
+    <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+    <div className='flex items-center gap-2 flex-1'>
+      <MapPin className='size-5 text-zinc-400'/>
+      <input 
+      disabled={isGuestsInputOpen} 
+      type="text" 
+      placeholder="Para onde você vai?" 
+      className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+      onChange={event => setDestination(event.target.value)}
+      />
+    </div>
+    
+    <button onClick={openDatePicker} disabled={isGuestsInputOpen} className='text-left flex items-center gap-2 w-60'>
+      <Calendar className='size-5 text-zinc-400'/>
+      <span 
+        className="text-lg text-zinc-400 w-40 flex-1">
+        {displayedDate || 'Quando?' }
+      </span>
+    </button>
+    {isDatePickerOpen && (
+      <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
+      <div className='rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+      <div className='space-y-2'>
+        <div className='flex items-center justify-between'>
+          <h2 className='text-lg font-semibold'>Selecione a data</h2>
+          <button onClick={closeDatePicker} type='button'>
+            <X className='size-5 text-zinc-400'/>
+          </button>
+        </div>
+  
+        </div>
+        <DayPicker mode="range" selected={eventStartAndEndDates} onSelect={setEventStartAndEndDates} />
+      </div>
+      
+    </div>
+    )}
+    
+
+      <div className='w-px h-6 bg-zinc-800'/>
+
+     {isGuestsInputOpen ?(
+      <Button onClick={closeGuestsInput} variant="secondary">
+        Alterar local/data
+        <Settings2 className='size-5'/>
+      </Button>
+    ):(
+      <Button onClick={openGuestsInput} variant="primary">
+        continuar
+        <ArrowRight className='size-5'/>
+      </Button>
+     ) }    
+    </div>
+  )
+}
